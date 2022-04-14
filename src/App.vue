1a512d6981e9e4660340e73e4d5a14f723053783
AcruxTech <acrux_tech@mail.ru>
2022-04-14 16:35:41 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+/* #nav a.router-link-exact-active {
+  color: #42b983;
+} */
+</style>
